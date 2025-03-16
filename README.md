9091ff50193b046f00bc5bfc21e120f46e309a25
michael-kirchner-at <michael.kirchner@welldone.cloud>
2025-03-16 09:10:51 +0100

    Added filter for default resources, Added resource properties to results

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # aws-list-resources
 
-Uses the AWS Cloud Control API to list resources that are present in a given AWS account and region(s). Discovered resources are written to a JSON output file. 
+Uses the AWS Cloud Control API to list resources that are present in a given AWS account and region(s). Discovered resources are written to a JSON result file. 
 
 
 ## Usage
@@ -20,7 +20,7 @@ python aws_list_resources.py --regions us-east-1,eu-central-1
 
 python aws_list_resources.py --regions ALL
 
-python aws_list_resources.py --regions ALL --include-resource-types AWS::EC2::*,AWS::DynamoDB::* --exclude-resource-types AWS::EC2::DHCPOptions,AWS::EC2::VPCGatewayAttachment
+python aws_list_resources.py --regions ALL --include-resource-types AWS::EC2::*,AWS::DynamoDB::*
 ```
 
 
@@ -29,21 +29,24 @@ python aws_list_resources.py --regions ALL --include-resource-types AWS::EC2::*,
 ```
 --exclude-resource-types  do not list the specified comma-separated resource types (supports wildcards)
 --include-resource-types  only list the specified comma-separated resource types (supports wildcards)
---only-show-counts        only show resource counts instead of listing their full identifiers
+--only-show-counts        only show resource counts instead of extended resource information
 --profile PROFILE         named AWS profile to use when running the command
 --regions REGIONS         comma-separated list of target AWS regions or 'ALL'
 ```
 
 
+## Example result
+
+Results are written to a JSON file ([see example](doc/example_results.json))
+
+
 ## Notes
 
-* The script can only discover resources that are currently supported by the AWS Cloud Control API and offer support for the `List` operation:
+* The script can only discover resources that are supported by the AWS Cloud Control API and offer support for the `List` operation:
 
   https://docs.aws.amazon.com/cloudcontrolapi/latest/userguide/supported-resources.html
-  
-  It is further restricted to those resources where the `List` operation does not expect any additional parameters.
 
-* The JSON output file will also contain default resources that were created by AWS, independent of whether you actually used the service or not.
+* The script filters out default resources that AWS provides in each account and that often cannot be modified or deleted. However, AWS may create new default resources any time that the script does not correctly filter yet. Please create an issue in case you notice missing filters.
 
 
 ## Minimum IAM permissions required
@@ -67,53 +70,3 @@ The script requires read access to all AWS services you want to list resources f
 }
 ```
 
-
-## Example output file
-
-Truncated example JSON output file:
-```json
-{
-  "_metadata": {
-    "account_id": "123456789012",
-    "account_principal": "arn:aws:iam::123456789012:user/myuser",
-    "invocation": "aws_list_resources.py --regions us-east-1,eu-central-1",
-    "run_timestamp": "20250105134120"
-    // ...
-  },
-  "regions": {
-    "us-east-1": {
-      "AWS::Athena::DataCatalog": [
-        "AwsDataCatalog"
-      ],
-      "AWS::CloudFront::CachePolicy": [
-        "08627262-05a9-4f76-9ded-b50ca2e3a84f",
-        "2e54312d-136d-493c-8eb9-b001f22f67d2",
-        "4135ea2d-6df8-44a3-9df3-4b5a84be39ad",
-        "658327ea-f89d-4fab-a63d-7e88639e58f6",
-        "b2884449-e4de-46a7-ac36-70bc7f1ddd6d"
-      ],
-      "AWS::EC2::DHCPOptions": [
-        "dopt-0aff9c4854b33dc5c"
-      ],
-      "AWS::EC2::InternetGateway": [
-        "igw-0090532d0f608e279"
-      ],
-      "AWS::EC2::NetworkAcl": [
-        "acl-0451d5fc3be271330"
-      ],
-      "AWS::EC2::RouteTable": [
-        "rtb-077ff6c625794e4fe"
-      ],
-      "AWS::IAM::Role": [
-        "AWSServiceRoleForCloudTrail",
-        "AWSServiceRoleForGlobalAccelerator",
-        "AWSServiceRoleForOrganizations",
-        "AWSServiceRoleForSupport",
-        "AWSServiceRoleForTrustedAdvisor",
-        "OrganizationAccountAccessRole"
-      ],
-      // ...
-    }
-  }
-}
-```
--- aws_list_resources.py
+++ aws_list_resources.py
@@ -34,6 +34,258 @@ class DeniedListOperationException(Exception):
     pass
 
 
+def apply_default_resources_filter(resource_type, resources):
+    """
+    Filters out default resources that are always present in an AWS account, independent of whether the respective
+    AWS service is actually used or not. Returns the list of remaining resources after applying the filter.
+    """
+    match resource_type:
+        case "AWS::AppConfig::DeploymentStrategy":
+            return {k: v for k, v in resources.items() if not k.startswith("AppConfig.")}
+
+        case "AWS::AppConfig::Extension":
+            return {k: v for k, v in resources.items() if "::extension/AWS." not in v["Arn"]}
+
+        case "AWS::AppRunner::AutoScalingConfiguration":
+            return {k: v for k, v in resources.items() if ":autoscalingconfiguration/DefaultConfiguration/" not in k}
+
+        case "AWS::AppRunner::ObservabilityConfiguration":
+            return {k: v for k, v in resources.items() if ":observabilityconfiguration/DefaultConfiguration/" not in k}
+
+        case "AWS::Athena::DataCatalog":
+            return {k: v for k, v in resources.items() if k != "AwsDataCatalog"}
+
+        case "AWS::Athena::WorkGroup":
+            return {k: v for k, v in resources.items() if k != "primary"}
+
+        case "AWS::Backup::BackupVault":
+            return {k: v for k, v in resources.items() if k != "Default"}
+
+        case "AWS::Cassandra::Keyspace":
+            return {k: v for k, v in resources.items() if k != "system_multiregion_info"}
+
+        case "AWS::CloudFormation::PublicTypeVersion":
+            return {k: v for k, v in resources.items() if "::type/" not in k}
+
+        case "AWS::CloudFront::CachePolicy":
+            default_resources = (
+                "08627262-05a9-4f76-9ded-b50ca2e3a84f",
+                "17322e93-4707-445a-93bc-6c8c16621822",
+                "1b1c9610-973a-4fb9-9932-a6c274840887",
+                "1c6db51a-a33f-469a-8245-dae26771f530",
+                "2e54312d-136d-493c-8eb9-b001f22f67d2",
+                "4135ea2d-6df8-44a3-9df3-4b5a84be39ad",
+                "4c02794c-7c81-4ba1-8b5d-e05fb0f95ed8",
+                "4cc15a8a-d715-48a4-82b8-cc0b614638fe",
+                "4d1d2f1d-3a71-49ad-9e08-7ea5d843a556",
+                "658327ea-f89d-4fab-a63d-7e88639e58f6",
+                "766eb028-1aff-4eb2-a5a4-2674e1538f26",
+                "7e5fad67-ee98-4ad0-b05a-394999eefc1a",
+                "83da9c7e-98b4-4e11-a168-04f0df8e2c65",
+                "a6bad946-36c3-4c33-aa98-362c74a7fb13",
+                "b2884449-e4de-46a7-ac36-70bc7f1ddd6d",
+            )
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::CloudFront::OriginRequestPolicy":
+            default_resources = (
+                "216adef6-5c7f-47e4-b989-5492eafa07d3",
+                "33f36d7e-f396-46d9-90e0-52428a34d9dc",
+                "59781a5b-3903-41f3-afcb-af62929ccde1",
+                "775133bc-15f2-49f9-abea-afb2e0bf67d2",
+                "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf",
+                "acba4595-bd28-49b8-b9fe-13317c0390fa",
+                "b689b0a8-53d0-40ab-baf2-68738e2966ac",
+            )
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::CloudFront::ResponseHeadersPolicy":
+            default_resources = (
+                "5cc3b908-e619-4b99-88e5-2cf7f45965bd",
+                "60669652-455b-4ae9-85a4-c4c02393f86c",
+                "67f7725c-6f97-4210-82d7-5512b31e9d03",
+                "e61eb60c-9c35-4d20-a928-2b84e02af89c",
+                "eaab4381-ed33-4a86-88ca-d9558dc6cd63",
+            )
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::CloudTrail::Dashboard":
+            return {k: v for k, v in resources.items() if v["Type"] != "MANAGED"}
+
+        case "AWS::CodeDeploy::DeploymentConfig":
+            return {k: v for k, v in resources.items() if not k.startswith("CodeDeployDefault.")}
+
+        case "AWS::CodePipeline::CustomActionType":
+            default_resources = (
+                "Approval|Manual|1",
+                "Build|CodeBuild|1",
+                "Build|ECRBuildAndPublish|1",
+                "Compute|Commands|1",
+                "Deploy|AlexaSkillsKit|1",
+                "Deploy|AppConfig|1",
+                "Deploy|CloudFormationStackInstances|1",
+                "Deploy|CloudFormationStackSet|1",
+                "Deploy|CloudFormation|1",
+                "Deploy|CodeDeployToECS|1",
+                "Deploy|CodeDeploy|1",
+                "Deploy|EC2|1",
+                "Deploy|ECS|1",
+                "Deploy|EKS|1",
+                "Deploy|ElasticBeanstalk|1",
+                "Deploy|OpsWorks|1",
+                "Deploy|S3|1",
+                "Deploy|ServiceCatalog|1",
+                "Invoke|CodePipeline|1",
+                "Invoke|InspectorScan|1",
+                "Invoke|Lambda|1",
+                "Invoke|Snyk|1",
+                "Invoke|StepFunctions|1",
+                "Source|CodeCommit|1",
+                "Source|CodeStarSourceConnection|1",
+                "Source|ECR|1",
+                "Source|GitHub|1",
+                "Source|S3|1",
+                "Test|CodeBuild|1",
+                "Test|DeviceFarm|1",
+                "Test|GhostInspector|1",
+                "Test|MFStormRunner|1",
+                "Test|Runscope|1",
+            )
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::EC2::PrefixList":
+            return {k: v for k, v in resources.items() if v["OwnerId"] != "AWS"}
+
+        case "AWS::ECS::CapacityProvider":
+            default_resources = ("FARGATE", "FARGATE_SPOT")
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::ElastiCache::ParameterGroup":
+            return {k: v for k, v in resources.items() if not k.startswith("default.")}
+
+        case "AWS::ElastiCache::User":
+            return {k: v for k, v in resources.items() if k != "default"}
+
+        case "AWS::Events::EventBus":
+            return {k: v for k, v in resources.items() if k != "default"}
+
+        case "AWS::GameLift::Location":
+            default_resources = (
+                "af-south-1",
+                "af-south-1-los-1",
+                "ap-east-1",
+                "ap-northeast-1",
+                "ap-northeast-2",
+                "ap-northeast-3",
+                "ap-south-1",
+                "ap-southeast-1",
+                "ap-southeast-2",
+                "ca-central-1",
+                "eu-central-1",
+                "eu-north-1",
+                "eu-south-1",
+                "eu-west-1",
+                "eu-west-2",
+                "eu-west-3",
+                "me-south-1",
+                "sa-east-1",
+                "us-east-1",
+                "us-east-1-atl-1",
+                "us-east-1-chi-1",
+                "us-east-1-dfw-1",
+                "us-east-1-iah-1",
+                "us-east-1-mci-1",
+                "us-east-2",
+                "us-west-1",
+                "us-west-2",
+                "us-west-2-den-1",
+                "us-west-2-lax-1",
+                "us-west-2-phx-1",
+            )
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::IAM::ManagedPolicy":
+            return {k: v for k, v in resources.items() if not k.startswith("arn:aws:iam::aws:policy/")}
+
+        case "AWS::IoT::DomainConfiguration":
+            default_resources = ("iot:CredentialProvider", "iot:Data-ATS", "iot:Jobs")
+            return {k: v for k, v in resources.items() if k not in default_resources}
+
+        case "AWS::KMS::Alias":
+            return {k: v for k, v in resources.items() if not k.startswith("alias/aws/")}
+
+        case "AWS::MediaLive::CloudWatchAlarmTemplate":
+            return {k: v for k, v in resources.items() if "::cloudwatch-alarm-template:aws-" not in v["Arn"]}
+
+        case "AWS::MediaLive::CloudWatchAlarmTemplateGroup":
+            return {k: v for k, v in resources.items() if "::cloudwatch-alarm-template-group:aws-" not in v["Arn"]}
+
+        case "AWS::MemoryDB::ACL":
+            return {k: v for k, v in resources.items() if k != "open-access"}
+
+        case "AWS::MemoryDB::ParameterGroup":
+            return {k: v for k, v in resources.items() if not k.startswith("default.")}
+
+        case "AWS::MemoryDB::User":
+            return {k: v for k, v in resources.items() if k != "default"}
+
+        case "AWS::RAM::Permission":
+            return {k: v for k, v in resources.items() if v["PermissionType"] != "AWS_MANAGED"}
+
+        case "AWS::RDS::DBClusterParameterGroup":
+            return {k: v for k, v in resources.items() if not k.startswith("default.")}
+
+        case "AWS::RDS::DBParameterGroup":
+            return {k: v for k, v in resources.items() if not k.startswith("default.")}
+
+        case "AWS::RDS::OptionGroup":
+            return {k: v for k, v in resources.items() if not k.startswith("default:")}
+
+        case "AWS::Route53Resolver::FirewallDomainList":
+            return {k: v for k, v in resources.items() if not v["CreatorRequestId"].startswith("AWSManaged")}
+
+        case "AWS::Route53Resolver::ResolverRule":
+            return {k: v for k, v in resources.items() if not k.startswith("rslvr-autodefined-")}
+
+        case "AWS::Route53Resolver::ResolverRuleAssociation":
+            return {k: v for k, v in resources.items() if not k.startswith("rslvr-autodefined-")}
+
+        case "AWS::S3::StorageLens":
+            return {k: v for k, v in resources.items() if k != "default-account-dashboard"}
+
+        case "AWS::SSM::Document":
+            default_prefixes = (
+                "AWS",
+                "AlertLogic",
+                "Amazon",
+                "Aws",
+                "CrowdStrike",
+                "Dynatrace",
+                "FalconSensor",
+                "New-Relic",
+                "SSM",
+                "TrendMicro",
+            )
+            return {
+                k: v for k, v in resources.items() if not any([k.startswith(prefix) for prefix in default_prefixes])
+            }
+
+        case "AWS::SSM::PatchBaseline":
+            return {k: v for k, v in resources.items() if not v["Name"].startswith("AWS-")}
+
+        case "AWS::Scheduler::ScheduleGroup":
+            return {k: v for k, v in resources.items() if k != "default"}
+
+        case "AWS::XRay::Group":
+            return {k: v for k, v in resources.items() if ":group/Default" not in k}
+
+        case "AWS::XRay::SamplingRule":
+            return {k: v for k, v in resources.items() if ":sampling-rule/Default" not in k}
+
+        case _:
+            return resources
+
+
 def get_supported_resource_types(cloudformation_client):
     """
     Returns a list of resource types that are supported in a region by querying the CloudFormation registry.
@@ -61,12 +313,12 @@ def get_resources(cloudcontrol_client, resource_type):
     API. If the API call failed, an empty list is returned. If the API call likely failed because of permission
     issues, a DeniedListOperationException is raised.
     """
-    collected_resources = []
+    collected_resources = {}
     list_resources_paginator = cloudcontrol_client.get_paginator("list_resources")
     try:
         for list_resources_page in list_resources_paginator.paginate(TypeName=resource_type):
             for resource in list_resources_page["ResourceDescriptions"]:
-                collected_resources.append(resource["Identifier"])
+                collected_resources[resource["Identifier"]] = json.loads(resource["Properties"])
 
     except Exception as ex:
         # There is unfortunately a long and non-uniform list of exceptions that can occur with the Cloud Control API,
@@ -84,6 +336,9 @@ def get_resources(cloudcontrol_client, resource_type):
 
 
 def log_error(msg, region):
+    """
+    Logs the given error message to both the result file and to stdout.
+    """
     result_collection["_metadata"]["errors"][region].append(msg)
     print("Error: {}".format(msg))
 
@@ -126,11 +381,12 @@ def analyze_region(region):
         try:
             print("Listing {}, region {}".format(resource_type, region))
             resources = get_resources(cloudcontrol_client, resource_type)
+            resources = apply_default_resources_filter(resource_type, resources)
             if resources:
                 if args.only_show_counts:
                     result_collection["regions"][region][resource_type] = len(resources)
                 else:
-                    result_collection["regions"][region][resource_type] = sorted(resources)
+                    result_collection["regions"][region][resource_type] = resources
 
         except DeniedListOperationException:
             msg = "Access denied to list resource type in region {}: {}".format(region, resource_type)
@@ -193,7 +449,7 @@ if __name__ == "__main__":
         "--only-show-counts",
         default=False,
         action="store_true",
-        help="only show resource counts instead of listing their full identifiers",
+        help="only show resource counts instead of extended resource information",
     )
     parser.add_argument(
         "--profile",
@@ -269,4 +525,4 @@ if __name__ == "__main__":
     with open(result_file, "w") as out_file:
         json.dump(result_collection, out_file, indent=2, sort_keys=True)
 
-    print("Output file written to {}".format(result_file))
+    print("Result file written to {}".format(result_file))
